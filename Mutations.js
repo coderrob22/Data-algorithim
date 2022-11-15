5133d51d0241a8bd82ef3887705f296c3c8f8990
coderrob22 <robertknott7@gmail.com>
2022-11-15 09:51:07 -0500

    is the 2nd array in the first?

---
+++ Mutations.js
@@ -0,0 +1,39 @@
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+// The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+// Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+
+
+//Loop
+function mutation(arr) {
+    const test = arr[1].toLowerCase();
+    const target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+
+//toLowercase, split, indexOf method
+function mutation(arr) {
+    return arr[1]
+      .toLowerCase()
+      .split("")
+      .every(function(letter) {
+        return arr[0].toLowerCase().indexOf(letter) !== -1;
+      });
+  }
+
+//Recursion
+function mutation([ target, test ], i = 0) {
+    target = target.toLowerCase();
+    test = test.toLowerCase();
+    return i >= test.length
+      ? true
+      : !target.includes(test[i])
+        ? false
+        : mutation([ target, test ], i + 1);
+  }
